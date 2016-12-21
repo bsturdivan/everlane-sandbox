4faf9cd80adac2742729523bd774191930f80480
Brian Sturdivan <bsturdivan@gmail.com>
2016-12-20 18:15:18 -0800

    Create basic carousel functionality

--- public/javascript/slider.js
+++ public/javascript/slider.js
@@ -1,14 +1,22 @@
 (function() {
-  $(document).ready(function() {
-    return new IScroll($('.slider')[0], {
+  window.onload = function() {
+    var scroll, width;
+    width = $('.slider__slide').outerWidth(true) * $('.slider__slide').length;
+    $('.slider__container').css('width', width);
+    scroll = new IScroll('.slider', {
       scrollX: true,
       scrollY: false,
       momentum: true,
+      bounceTime: 600,
+      deceleration: 0.002,
       snap: true,
       snapSpeed: 400,
       keyBindings: true,
       indicators: false
     });
-  });
+    return document.addEventListener('touchmove', function(e) {
+      return e.preventDefault();
+    }, false);
+  };
 
 }).call(this);
