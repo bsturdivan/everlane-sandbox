522a3ce3a1229bacb6701d348822b06bc92ebe1f
Brian Sturdivan <bsturdivan@gmail.com>
2017-01-10 15:33:12 -0800

    Add gulp error handling

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
 const gulp = require('gulp');
+const notify = require('gulp-notify');
 const sass = require('gulp-sass');
 const coffee = require('gulp-coffee');
 const connect = require('gulp-connect');
 
+// ERROR HANDLING
+function onError(error) {
+  notify.onError({
+    title: 'Error',
+    message: error.message
+  })(error);
+  this.emit('end');
+}
+
+// TASKS
 gulp.task('sass', function() {
   return gulp.src('./app/stylesheets/**/*.scss')
     .pipe(sass())
+    .on('error', onError)
     .pipe(gulp.dest('./public/css'))
     .pipe(connect.reload())
 });
@@ -13,12 +25,14 @@ gulp.task('sass', function() {
 gulp.task('css', function() {
   return gulp.src('./app/stylesheets/**/*.css')
     .pipe(gulp.dest('./public/stylesheets'))
+    .on('error', onError)
     .pipe(connect.reload())
 });
 
 gulp.task('coffee', function() {
   return gulp.src('./app/javascript/**/*.coffee')
     .pipe(coffee())
+    .on('error', onError)
     .pipe(gulp.dest('./public/javascript'))
     .pipe(connect.reload())
 });
@@ -26,18 +40,21 @@ gulp.task('coffee', function() {
 gulp.task('js', function() {
   return gulp.src('./app/javascript/**/*.js')
     .pipe(gulp.dest('./public/javascript'))
+    .on('error', onError)
     .pipe(connect.reload())
 });
 
 gulp.task('html-layout', function() {
   return gulp.src('./app/views/layout/**/*.html')
     .pipe(gulp.dest('./public'))
+    .on('error', onError)
     .pipe(connect.reload())
 });
 
 gulp.task('html-pages', function() {
   return gulp.src('./app/views/pages/**/*.html')
     .pipe(gulp.dest('./public'))
+    .on('error', onError)
     .pipe(connect.reload())
 });
 
